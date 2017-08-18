4d3b16bb90b10c9a5445629650eb7550be8cdb38
alayor <alayor3@gmail.com>
2017-08-17 19:08:06 -0500

    Modify startup script for macos

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "::::::::::STARTING CONFIGURATION SERVICE:::::::::::"
-cd %~dp0
-call gradlew build -x test
-call java -jar build/libs/configuration-service-0.0.1-SNAPSHOT.jar
